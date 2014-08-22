5b59b1b6afb8380e4bd8eec98fd726bd3505219a
Joseph Pietruch <percipient24@gmail.com>
2014-08-22 00:13:54 -0400

    Setting up for an Angular App. Playing with Bourbon Neat.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comicBlindAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the comicBlindAppApp
+ */
+angular.module('comicBlindAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
